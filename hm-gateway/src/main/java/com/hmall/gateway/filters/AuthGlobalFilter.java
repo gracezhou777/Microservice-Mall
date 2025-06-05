a8d9a3b87e1c0d4d0e3a87edbb1ee88c0fcfa50e
Grace Zhou <zhoujingshu97@gmail.com>
2025-06-04 20:46:55 -0700

    interceptor

--- hm-gateway/src/main/java/com/hmall/gateway/filters/AuthGlobalFilter.java
+++ hm-gateway/src/main/java/com/hmall/gateway/filters/AuthGlobalFilter.java
@@ -57,10 +57,13 @@ public class AuthGlobalFilter implements GlobalFilter, Ordered {
         }
 
         //5. TODO 传递用户信息
-        System.out.println("userId = " + userId);
+        String userInfo = userId.toString();
+        ServerWebExchange swe = exchange.mutate()
+                .request(b -> b.header("user-info", userInfo))
+                .build();
 
         //6. 放行
-        return chain.filter(exchange);
+        return chain.filter(swe);
     }
 
     private boolean isExclude(String path) {
